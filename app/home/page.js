74b3d5b89bd68329a1ea6100894c62a8a5b9deb6
gani <saiganesh7989@gmail.com>
2024-10-05 13:48:56 +0530

    patch#apos

--- app/home/page.js
+++ app/home/page.js
@@ -5,6 +5,7 @@ import { motion, useAnimation } from "framer-motion";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, Loader, useGLTF } from "@react-three/drei";
 import Navbar from "../Components/Navbar";
+import Image from 'next/image';
 
 // Component for 3D object
 const Model = () => {
@@ -26,8 +27,7 @@ const HomePage = () => {
   useEffect(() => {
     const handleScroll = () => {
       const scrollY = window.scrollY;
-      const scrollPercent =
-        scrollY / (document.body.scrollHeight - window.innerHeight);
+      const scrollPercent = scrollY / (document.body.scrollHeight - window.innerHeight);
       controlsAnimation.start({ opacity: 1 - scrollPercent });
     };
 
@@ -82,18 +82,12 @@ const HomePage = () => {
               transition={{ duration: 1, delay: 1 }}
             >
               <div className="text-center">
-                <h2 className="text-2xl font-bold mb-4">
-                  What's an Opportunity
-                </h2>
+                <h2 className="text-2xl font-bold mb-4">What's an Opportunity</h2>
                 <p>
-                  We are a team of passionate individuals who believe in the
-                  power of second chances. We are here to help you succeed and
-                  thrive in your career.&apos;
+                  We are a team of passionate individuals who believe in the power of second chances. We are here to help you succeed and thrive in your career's journey.
                 </p>
               </div>
             </motion.div>
-
-            {/* New Section: What We're Looking For */}
           </div>
 
           <Loader />
@@ -101,76 +95,75 @@ const HomePage = () => {
       </div>
 
       <div>
-  <motion.h1
-    className="relative mx-auto w-full max-w-4xl text-4xl font-light mb-4 text-ellipsis text-center py-10"
-    initial={{ opacity: 0 }}
-    animate={{ opacity: 1 }}
-    transition={{ duration: 1, delay: 1 }}
-  >
-    Our Values
-  </motion.h1>
+        <motion.h1
+          className="relative mx-auto w-full max-w-4xl text-4xl font-light mb-4 text-ellipsis text-center py-10"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 1 }}
+        >
+          Our Values
+        </motion.h1>
 
-  <div className="flex flex-col md:flex-row w-full p-10">
-    {[
-      {
-        title: "Rapid Growth",
-        description:
-          "Our application support team is dedicated to assisting you with any inquiries or issues you may encounter. Whether you need help getting started, troubleshooting, or understanding features, we are here to help you make the most of our application.",
-        imgSrc: "/images/projects.png",
-        altText: "Graphic representing Application Support",
-      },
-      {
-        title: "Grow Network",
-        description:
-          "We prioritize your needs and strive to exceed your expectations. Your feedback helps us improve and innovate continuously.",
-        imgSrc: "/images/projects.png",
-        altText: "Graphic representing Customer Satisfaction",
-      },
-    ].map((value, index) => (
-      <motion.div
-        key={index}
-        className="flex flex-col md:flex-row items-center w-full max-w-4xl mb-6"
-        initial={{ y: 10, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.5 }}
-      >
-        <motion.img
-          src={value.imgSrc}
-          alt={value.altText}
-          className="w-full md:w-1/2 rounded-lg md:rounded-l-lg mb-4 md:mb-0"
-          whileHover={{ scale: 1.05, rotate: 2 }} // Jitter on hover
-        />
-        <div className="w-full md:w-1/2 pl-0 md:pl-6">
-          <h2 className="text-3xl font-bold mb-2">{value.title}</h2>
-          <p>{value.description}</p>
+        <div className="flex flex-col md:flex-row w-full p-10">
+          {[
+            {
+              title: "Rapid Growth",
+              description: "Our application support team is dedicated to assisting you with any inquiries or issues you may encounter. Whether you need help getting started, troubleshooting, or understanding features, we are here to help you make the most of our application.",
+              imgSrc: "/images/projects.png",
+              altText: "Graphic representing Application Support",
+            },
+            {
+              title: "Grow Network",
+              description: "We prioritize your needs and strive to exceed your expectations. Your feedback helps us improve and innovate continuously.",
+              imgSrc: "/images/projects.png",
+              altText: "Graphic representing Customer Satisfaction",
+            },
+          ].map((value, index) => (
+            <motion.div
+              key={index}
+              className="flex flex-col md:flex-row items-center w-full max-w-4xl mb-6"
+              initial={{ y: 10, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ duration: 0.5 }}
+            >
+              <div style={{ position: 'relative', width: '100%', height: '300px' }}>
+                <Image
+                  src={value.imgSrc}
+                  alt={value.altText}
+                  fill
+                  style={{ objectFit: 'cover' }}
+                />
+              </div>
+              <div className="w-full md:w-1/2 pl-0 md:pl-6">
+                <h2 className="text-3xl font-bold mb-2">{value.title}</h2>
+                <p>{value.description}</p>
+              </div>
+            </motion.div>
+          ))}
         </div>
-      </motion.div>
-    ))}
-  </div>
 
-  <div className="flex justify-center items-center w-full">
-    <motion.div
-      className="w-full max-w-2xl text-center"
-      initial={{ y: 10, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
-      transition={{ duration: 0.5, delay: 0.4 }}
-    >
-      <motion.img
-        src="/images/marketer.png"
-        alt="Team Work"
-        className="w-full rounded-lg"
-        whileHover={{ scale: 1.05, rotate: 2 }} // Jitter on hover
-      />
-      <h2 className="text-2xl font-bold mt-4">Be in trend</h2>
-      <p>
-        Collaboration is key to our success. Our dedicated team works
-        together to deliver exceptional results and ensure your
-        satisfaction.
-      </p>
-    </motion.div>
-  </div>
-</div>
-
+        <div className="flex justify-center items-center w-full">
+          <motion.div
+            className="w-full max-w-2xl text-center"
+            initial={{ y: 10, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.5, delay: 0.4 }}
+          >
+            <div style={{ position: 'relative', width: '100%', height: '300px' }}>
+              <Image
+                src="/images/marketer.png"
+                alt="Team Work"
+                fill
+                style={{ objectFit: 'cover' }}
+              />
+            </div>
+            <h2 className="text-2xl font-bold mt-4">Be in trend</h2>
+            <p>
+              Collaboration is key to our success. Our dedicated team works together to deliver exceptional results and ensure your satisfaction.
+            </p>
+          </motion.div>
+        </div>
+      </div>
     </>
   );
 };
